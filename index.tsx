e7eeb6bcd7da2bbd7fb99b8b574ba67f6c41da23
Blake Smith <blakeinvictoria@gmail.com>
2025-12-07 12:31:27 -0800

    feat: add icons

--- index.tsx
+++ index.tsx
@@ -5,22 +5,30 @@ manifest.assign({
     description: "Copy things easiliy",
     version: "0.0.1",
     permissions: ['clipboardWrite', 'offscreen', 'tabs'],
-    action: async () => {
-        const tab = (await chrome.tabs.query({ active: true, currentWindow: true }))[0];
-        const mdLink = `[${tab?.title}](${tab?.url})`
+    action: {
+        default_icon: {
+            16: "icons/clipboard-16.png",
+            32: "icons/clipboard-32.png",
+            48: "icons/clipboard-48.png",
+            128: "icons/clipboard-128.png"
+        },
+        onClick: async () => {
+            const tab = (await chrome.tabs.query({ active: true, currentWindow: true }))[0];
+            const mdLink = `[${tab?.title}](${tab?.url})`
 
-        await chrome.offscreen.createDocument({
-            url: 'offscreen.html',
-            reasons: ['CLIPBOARD'],
-            justification: 'Copying to clipboard',
-        });
+            await chrome.offscreen.createDocument({
+                url: 'offscreen.html',
+                reasons: ['CLIPBOARD'],
+                justification: 'Copying to clipboard',
+            });
 
-        await chrome.runtime.sendMessage({
-            type: 'copy',
-            data: mdLink,
-        });
+            await chrome.runtime.sendMessage({
+                type: 'copy',
+                data: mdLink,
+            });
 
-        await chrome.offscreen.closeDocument();
+            await chrome.offscreen.closeDocument();
+        }
     }
 })
 
